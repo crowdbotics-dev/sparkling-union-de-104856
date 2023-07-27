2379b14c77a8d3a9523cab07491dce970dc52281
Crowdbotics <engineering@crowdbotics.com>
2023-07-27 12:13:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sparkling-union-de-104856",
+  url: "https://sparkling-union-de-104856.botics.co",
+  api: "https://sparkling-union-de-104856.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
